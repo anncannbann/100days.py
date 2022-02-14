01f032352a7f2397d2fb9d27b581f63cd4990066
anncannbann <anncannbann@gmail.com>
2022-02-14 23:23:21 +0530

    First day complete

---
+++ day-1/test.py
@@ -0,0 +1,35 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+temp =a
+a = b
+b= temp
+
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + str(a))
+print("b: " + str(b))
+
+#band name generator
+#1. Create a greeting for your program.
+print('Hello There!')
+#2. Ask the user for the city that they grew up in.
+city = input('Which city did you grow up in?\n')
+
+#3. Ask the user for the name of a pet.
+pet = input('What is your pets name?\n')
+
+#4. Combine the name of their city and pet and show them their band name.
+print('Your band name could be '+pet +" "+ city)
+
+#   https://band-name-generator-end.appbrewery.repl.run/
+#ok
